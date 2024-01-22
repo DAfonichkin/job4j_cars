b3abcd1827f2a685c22bb9a5c4acdf46dc91c807
Dmitriy Afonichkin <dafonichkin@gmail.com>
2024-01-23 00:01:31 +0300

    2. ManyToMany [#331986 #475751]

--- db/scripts/005_ddl_create_participates_table.sql
+++ db/scripts/005_ddl_create_participates_table.sql
@@ -2,5 +2,5 @@ CREATE TABLE participates (
    id serial PRIMARY KEY,
    post_id int not null REFERENCES auto_post(id),
    user_id int not null REFERENCES auto_user(id),
-   UNIQUE (item_id, user_id)
+   UNIQUE (post_id, user_id)
 );

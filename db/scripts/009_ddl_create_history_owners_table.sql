a45ca150545441a8c643f4992ee37c346862ac9a
Dmitriy Afonichkin <dafonichkin@gmail.com>
2024-02-19 23:29:00 +0300

    6. Модели и связи. Машины и владельцы [#4744 #482266]

--- db/scripts/009_ddl_create_history_owners_table.sql
+++ db/scripts/009_ddl_create_history_owners_table.sql
@@ -1,7 +1,7 @@
 create table history_owner(
     id serial primary key,
     owner_id int not null references owners (id),
-    car_id int not null references cars (id),
+    car_id int not null references car (id),
     startAt timestamp,
     endAt timestamp
 );
